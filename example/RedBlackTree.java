ae924e83ae22d8edf2f13b204ac50b57ac6fe504
natalya_kosyakina <anckamarckowa@yandex.ru>
2023-09-16 15:23:35 +0300

    Дерево доделано

--- example/RedBlackTree.java
+++ example/RedBlackTree.java
@@ -16,6 +16,8 @@ public class RedBlackTree<T extends Comparable<T>>{
         }
         else {
             add(value, root);
+            rebalance(root);
+            root.red = false;
         }
     }
     private boolean add(T value, Node node){
@@ -33,7 +35,7 @@ public class RedBlackTree<T extends Comparable<T>>{
             } else {
                 if (node.rightchild != null) {
                     add(value, node.rightchild);
-                    // Rebalance();
+                    rebalance(root);
                 } else {
                     node.rightchild = new Node();
                     node.rightchild.value = value;
@@ -58,5 +60,53 @@ public class RedBlackTree<T extends Comparable<T>>{
         if (node.value.compareTo(value) > 0){ // если искомое значение меньше значения текущего узла
             return find(value, node.leftchild);
         } else {return find(value, node.rightchild);}
+    }
+
+    private void rebalance(Node node){
+        Node result = node;
+        boolean imbalanced = true;
+        while (imbalanced) {
+            imbalanced = false;
+            if (result.rightchild != null && result.rightchild.red &&
+                    (result.leftchild == null || !result.red)) {
+                imbalanced = true;
+                result = rigthturn(result);
+            }
+            if (result.leftchild != null && result.red &&
+                    result.leftchild.leftchild != null && result.leftchild.leftchild.red) {
+                imbalanced = true;
+                result = leftturn(result);
+            }
+            if (result.leftchild != null && result.red && result.rightchild != null && result.rightchild.red) {
+                imbalanced = true;
+                colorswap(result);
+            }
+        }
+    }
+
+    private Node leftturn(Node node){
+        Node leftch = node.leftchild;
+        Node middle = leftch.rightchild;
+        leftch.rightchild = node;
+        node.leftchild = middle;
+        leftch.red = node.red;
+        node.red = true;
+        return leftch;
+    }
+
+    private Node rigthturn(Node node){
+        Node rightch = node.rightchild;
+        Node middle = rightch.leftchild;
+        rightch.leftchild = node;
+        node.rightchild = middle;
+        rightch.red = node.red;
+        node.red = true;
+        return rightch;
+    }
+
+    private void colorswap(Node node){
+        node.leftchild.red = false;
+        node.rightchild.red = false;
+        node.red = true;
     }
 }
